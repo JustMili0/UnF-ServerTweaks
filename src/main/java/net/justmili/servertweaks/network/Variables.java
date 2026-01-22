d4233d734d39a4f377b79f8a768bf15a43b2bcc8
JustMili <rodx160@gmail.com>
2026-01-23 00:44:22 +0100

    Uhh variables step 1

--- src/main/java/net/justmili/servertweaks/network/Variables.java
+++ src/main/java/net/justmili/servertweaks/network/Variables.java
@@ -1,4 +1,28 @@
 package net.justmili.servertweaks.network;
 
-public class Variables {
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import org.ladysnake.cca.api.v3.component.Component;
+import org.ladysnake.cca.api.v3.component.sync.AutoSyncedComponent;
+
+public class Variables implements Component, AutoSyncedComponent {
+    private boolean scaleLocked = false;
+
+    public boolean isScaleLocked() {
+        return scaleLocked;
+    }
+
+    public void setScaleLocked(boolean locked) {
+        this.scaleLocked = locked;
+    }
+
+    @Override
+    public void readFromNbt(CompoundTag tag, HolderLookup.Provider provider) {
+        scaleLocked = tag.getBoolean("isScaleLocked").orElse(false);
+    }
+
+    @Override
+    public void writeToNbt(CompoundTag tag, HolderLookup.Provider provider) {
+        tag.putBoolean("isScaleLocked", scaleLocked);
+    }
 }
